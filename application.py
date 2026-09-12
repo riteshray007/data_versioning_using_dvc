def225fbf0145cde70dd840c3ae94feb25e3d549
riteshray007 <ray.ritesh007@gmail.com>
2026-09-12 15:51:16 +0530

    second version of data

--- application.py
+++ application.py
@@ -9,6 +9,11 @@ data = {
 
 df = pd.DataFrame(data)
 
+
+# adding new row to df
+new_row = { "name" : "rajesh" , "age" : 30 , "city" : "cuttack" }
+df.loc[len(df.index)] = new_row
+
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 
@@ -17,6 +22,8 @@ file_path = os.path.join(data_dir , 'sample_data.csv')
 df.to_csv(file_path , index=False)
 
 print(f"csv file saved to {file_path}")
+
+
 
 
 
